0fd43cc4d45941115f72cb98e7a5e39d099cbf05
yanshay <yanshay>
2025-03-12 16:37:29 +0200

    first version of multi-printer support with tag including serial but w/o multi-printer tag processing

--- device/src/web_app.rs
+++ device/src/web_app.rs
@@ -1,9 +1,11 @@
 use core::cell::RefCell;
 use core::future::ready;
+use core::net::Ipv4Addr;
 
 use alloc::format;
 use alloc::rc::Rc;
 use alloc::string::{String, ToString};
+use alloc::vec::Vec;
 use picoserve::response::Redirect;
 use picoserve::routing::get;
 use picoserve::{
@@ -22,7 +24,7 @@ use framework::{
     prelude::*,
 };
 
-use crate::app_config::AppConfig;
+use crate::app_config::{AppConfig, DefaultPrinterConfig, PrinterConfig, PrintersConfig};
 
 pub struct NestedAppBuilder {
     pub framework: Rc<RefCell<Framework>>,
@@ -53,8 +55,9 @@ impl AppWithStateBuilder for NestedAppBuilder {
         let router = router.route(
             "/api/printer-config",
             post(
-                move |State(Encryption(key)): State<Encryption>, PrinterConfigDTO { ip, serial, name, access_code }| {
-                    ready(match app_config_clone_post.borrow_mut().set_printer_config(ip, name, serial, access_code) {
+                move |State(Encryption(key)): State<Encryption>, printers_config_dto: PrintersConfigDTO| {
+                    let default_printer_serial = printers_config_dto.default_printer_serial.clone();
+                    ready(match app_config_clone_post.borrow_mut().set_printers_config(printers_config_dto.into(), DefaultPrinterConfig {serial: default_printer_serial} ) {
                         Ok(_) => SetConfigResponseDTO { error_text: None }.encrypt(&key.borrow()),
                         Err(e) => SetConfigResponseDTO {
                             error_text: Some(format!("{e:?}")),
@@ -64,14 +67,14 @@ impl AppWithStateBuilder for NestedAppBuilder {
                 },
             )
             .get(move |State(Encryption(key)): State<Encryption>| {
-                ready(
-                    PrinterConfigDTO {
-                        ip: app_config_clone_get.borrow().configured_printer_ip.map(|v| v.to_string()) .unwrap_or(String::from("")),
-                        name: app_config_clone_get.borrow().configured_printer_name.clone().unwrap_or(String::from("")),
-                        serial: app_config_clone_get.borrow().printer_serial.clone().unwrap_or(String::from("")),
-                        access_code: app_config_clone_get.borrow().printer_access_code.clone().unwrap_or(String::from("")),
+                ready( {
+                        let borrowed_app_config = app_config_clone_get.borrow(); // notice the borrow, can't async here
+                        let printers = &borrowed_app_config.configured_printers;
+                        let default_printer = &borrowed_app_config.configured_default_printer;
+                        let mut printers_config = PrintersConfigDTO::from(printers);
+                        printers_config.default_printer_serial = default_printer.serial.clone();
+                        printers_config.encrypt(&key.borrow())
                     }
-                    .encrypt(&key.borrow()),
                 )
             }),
         );
@@ -105,12 +108,60 @@ impl AppWithStateBuilder for NestedAppBuilder {
 
 #[derive(serde::Deserialize, serde::Serialize)]
 struct PrinterConfigDTO {
-    ip: String,
-    name: String,
-    serial: String,
-    access_code: String,
+    ip: Option<String>,
+    name: Option<String>,
+    serial: Option<String>,
+    access_code: Option<String>,
 }
 encrypted_input!(PrinterConfigDTO);
+impl From<PrinterConfigDTO> for PrinterConfig {
+    fn from (v: PrinterConfigDTO) -> Self {
+        Self {
+            ip: v.ip.and_then(|s| s.parse::<Ipv4Addr>().ok()),
+            name: v.name, 
+            serial: v.serial,
+            access_code: v.access_code,
+        }
+    }
+}
+impl From<&PrinterConfig> for PrinterConfigDTO {
+    fn from (v: &PrinterConfig) -> Self {
+        Self {
+            ip: v.ip.and_then(|ip| Some(ip.to_string())),
+            name: v.name.clone(), 
+            serial: v.serial.clone(),
+            access_code: v.access_code.clone(),
+        }
+    }
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+struct PrintersConfigDTO {
+    printers: Vec<PrinterConfigDTO>,
+    default_printer_serial: Option<String>,
+}
+encrypted_input!(PrintersConfigDTO);
+impl From<PrintersConfigDTO> for PrintersConfig {
+    fn from (v: PrintersConfigDTO) -> Self {
+        Self {
+            printers: v.printers
+                .into_iter()
+                .map(PrinterConfig::from) // Convert each Printer to PrinterDTO
+                .collect(),
+        }
+    }
+}
+impl From<&PrintersConfig> for PrintersConfigDTO {
+    fn from (v: &PrintersConfig) -> Self {
+        Self {
+            printers: v.printers
+                .iter()
+                .map(PrinterConfigDTO::from) // Convert each Printer to PrinterDTO
+                .collect(),
+            default_printer_serial: None,
+        }
+    }
+}
 
 #[derive(serde::Deserialize, serde::Serialize)]
 struct TagConfigDTO {
